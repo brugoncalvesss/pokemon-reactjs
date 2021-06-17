b0fa16b8528eb201e080ecd8d8bb4b687fa8e4b5
Bruno Gonçalves <bcgonca@gmail.com>
2021-06-17 20:54:07 -0300

    Change API endpoint

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Api from '../services/Api'
+import PokemonType from './PokemonType'
 
 const PokemonCard = ({name}) => {
 
     const [pokemon, setPokemon] = useState([])
 
     const getPokemon = async (name) => {
-        const res = await Api.get(`pokemon-form/${name}`)
+        const res = await Api.get(`pokemon/${name}`)
         setPokemon(res.data)
     }
 
@@ -18,45 +19,36 @@ const PokemonCard = ({name}) => {
     return (
         <>
             <div className="col">
-                {pokemon ? (
-                    <div className="card card-pokemon mb-3">
-                        <div className="card-body">
+                <div className="card card-pokemon mb-3">
+                    <div className="card-body">
 
-                            <div className="d-flex align-items-center">
-                                <div className="me-3">
-                                {pokemon.sprites ? (
-                                    <img
-                                        src={pokemon.sprites.front_default}
-                                        className="img-fluid"
-                                        alt={pokemon.name}
-                                    />
-                                ) : (
-                                    <div className="spinner-border"></div>
-                                )}
+                        <div className="d-flex align-items-center">
+                            <div className="me-3">
+                            {pokemon.sprites ? (
+                                <img
+                                    src={pokemon.sprites.front_default}
+                                    className="img-fluid"
+                                    alt={pokemon.name}
+                                />
+                            ) : (
+                                <div className="spinner-border"></div>
+                            )}
+                            </div>
+                            <div>
+                                <div className="mb-2">
+                                    <Link to={`/pokemon/${pokemon.name}/`} className="text-decoration-none text-capitalize">
+                                        <strong>{pokemon.name}</strong>
+                                    </Link>
                                 </div>
-                                <div>
-                                    <div className="mb-2">
-                                        <Link to={`/pokemon/${pokemon.name}/`} className="text-decoration-none text-capitalize">
-                                            <strong>{pokemon.name}</strong>
-                                        </Link>
-                                    </div>
 
-                                    <div>
-                                        <div className="label small text-muted">
-                                            Type
-                                        </div>
-                                        <div className="text-capitalize">
-                                        {pokemon.id}
-                                        </div>
-                                    </div>
+                                <div>
+                                    {/* <PokemonType types={pokemon.types} /> */}
                                 </div>
                             </div>
-
                         </div>
+
                     </div>
-                ) : (
-                    <div className="spinner-border"></div>
-                )}
+                </div>
             </div>
         </>
     )
